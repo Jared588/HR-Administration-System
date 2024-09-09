a589812287547185b8fe30e164cdc14a4453f51f
Jared588 <jaredeprice@gmail.com>
2024-09-09 20:45:41 +0200

    Improve layout

--- src/app/employees/page.tsx
+++ src/app/employees/page.tsx
@@ -6,15 +6,11 @@ export default function EmployeeList() {
   return (
     <div className="flex h-screen w-full flex-col items-center">
       <Header />
-      <h1>Employees</h1>
-      <div className="flex w-full flex-col items-center border border-black p-6 md:w-3/5">
-        <h2 className="self-start">Filters</h2>
-        <FilterForm />
-      </div>
-      <div>
-        <div>
-          <p>Show per page</p>
-          <input type="text" className="border border-black" />
+      <div className="md:w-2/3">
+        <h1 className="text-2xl py-4">Employees</h1>
+        <div className="flex w-full flex-col items-center rounded-md border border-black p-4 mb-10">
+          <h2 className="self-start">Filters</h2>
+          <FilterForm />
         </div>
         <EmployeeTable />
       </div>
